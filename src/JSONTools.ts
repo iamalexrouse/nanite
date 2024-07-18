b2db1801fa203799d568404b7dbacbb54f160c64
Alex Rouse <123724383+ALEXWARELLC@users.noreply.github.com>
2024-07-18 15:24:59 +0800

    Added basic JSON validation.

--- src/JSONTools.ts
+++ src/JSONTools.ts
@@ -1,3 +1,12 @@
 export function ReturnAsJsonObject(Input: string): any {
     return JSON.parse(Input) as object;
 }
+
+export function IsValidJSON(Input: string): boolean {
+    try {
+        JSON.parse(Input);
+        return true;
+    } catch {
+        return false;
+    }
+}
--- src/tests/JSONTools.test.ts
+++ src/tests/JSONTools.test.ts
@@ -1,9 +1,17 @@
 import {describe, expect, test} from '@jest/globals';
-import {ReturnAsJsonObject} from '../JSONTools';
+import {IsValidJSON, ReturnAsJsonObject} from '../JSONTools';
 
 describe('JSONTools.js', () => {
     test("Parse's a JSON string and extracts the 'name' object.", () => {
         const nameResult = ReturnAsJsonObject('{"name":"alex"}');
         expect(nameResult['name']).toBe('alex');
+    });
+    
+    test("Checks if the given string is valid JSON. This should return true.", () => {
+        expect(IsValidJSON('{"name":"alex"}')).toBe(true);
+    });
+
+    test("Checks if the given string is valid JSON. This should return false.", () => {
+        expect(IsValidJSON('This is not valid JSON.')).toBe(false);
     });
 });
