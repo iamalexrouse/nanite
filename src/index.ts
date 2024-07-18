00de18f23487fa0b859ccb9407fd3a4e0a64a079
Alex Rouse <123724383+ALEXWARELLC@users.noreply.github.com>
2024-07-18 15:16:54 +0800

    Update Pacakge & remove `console.log` from test.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export function Base64ToJSON(Input: string): string {
 export function JSONToBase64(Input: string): string {
     try {
         const content = JSON.parse(Input);
-        console.log(JSON.stringify(content));
         return Buffer.from(JSON.stringify(content)).toString('base64');
     } catch (e) {
         throw e;
