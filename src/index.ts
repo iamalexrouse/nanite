40bb9f5a85c9454b1ab08f1eda49c6c3150e2486
Alex Rouse <123724383+ALEXWARELLC@users.noreply.github.com>
2024-07-31 17:19:29 +0800

    Added main file doumentation.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { IEncodingResult } from './IEncodingResult';
 
+/**
+ * @deprecated Base64ToJSON is no longer supported, Use 'FromBase64' instead.
+ * @param Input The Base64 string to convert to Json.
+ * @returns A converted Json string.
+ */
 export function Base64ToJSON(Input: string): string {
     return Buffer.from(Input, 'base64').toString();
 }
 
+/**
+ * @deprecated JSONToBase64 is no longer supported. Use `FromJson` instead.
+ * @param Input The Json string to convert to Base64.
+ * @returns A converted Base64 string.
+ */
 export function JSONToBase64(Input: string): string {
     try {
         const content = JSON.parse(Input);
@@ -16,6 +26,12 @@ export function JSONToBase64(Input: string): string {
 // !! DO NOT USE JUST YET !!
 // I need to finish rewriting some methods then the transition will happen.
 
+/**
+ * Converts an encoded Base64 string into a Json string.
+ * @param Input The Base64 string to convert to Json.
+ * @param Encoding The encoding scheme to use.
+ * @returns An encoded Json string.
+ */
 export function FromBase64(Input: string, Encoding: BufferEncoding = 'utf-8'): IEncodingResult {
     try {
         return { result: true, message: Buffer.from(Input, 'base64').toString(Encoding) }
@@ -24,6 +40,11 @@ export function FromBase64(Input: string, Encoding: BufferEncoding = 'utf-8'): I
     }
 }
 
+/**
+ * 
+ * @param Input The Json string to encode to Base64.
+ * @returns Great an encoded Base64 string.
+ */
 export function FromJson(Input: string): IEncodingResult {
     try {
         const content = JSON.parse(Input);
